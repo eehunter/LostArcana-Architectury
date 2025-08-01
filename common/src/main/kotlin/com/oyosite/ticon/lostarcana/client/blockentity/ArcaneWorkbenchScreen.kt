b15a1ad285ee10afb6e9d7789b4d1bfa7656ec38
Ned Hunter <eehuntercats@gmail.com>
2025-08-01 00:14:33 -0400

    Render Item mouseover tooltip on ArcaneWorkbenchScreen.

--- common/src/main/kotlin/com/oyosite/ticon/lostarcana/client/blockentity/ArcaneWorkbenchScreen.kt
+++ common/src/main/kotlin/com/oyosite/ticon/lostarcana/client/blockentity/ArcaneWorkbenchScreen.kt
@@ -26,6 +26,11 @@ class ArcaneWorkbenchScreen(menu: ArcaneWorkbenchMenu, inventory: Inventory, nam
         guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
     }
 
+    override fun render(guiGraphics: GuiGraphics, i: Int, j: Int, f: Float) {
+        super.render(guiGraphics, i, j, f)
+        renderTooltip(guiGraphics, i, j)
+    }
+
     override fun init() {
         super.init()
         //titleLabelX = (imageWidth - title)

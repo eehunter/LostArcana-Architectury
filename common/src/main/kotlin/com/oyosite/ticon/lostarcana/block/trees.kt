adf573f0919ccc57785857d834e9c51b2dc9a14c
Ned Hunter <eehuntercats@gmail.com>
2025-08-25 22:24:34 -0400

    Fix Greatwood Saplings.

--- common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/trees.kt
+++ common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/trees.kt
@@ -1,14 +1,14 @@
 package com.oyosite.ticon.lostarcana.block
 
+import com.oyosite.ticon.lostarcana.LostArcana
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
-import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.block.grower.TreeGrower
 import java.util.Optional
 
 val GREATWOOD_FEATURE = ResourceKey.create(
     Registries.CONFIGURED_FEATURE,
-    ResourceLocation.withDefaultNamespace("greatwood")
+    LostArcana.id("greatwood")
 )
 
 val GREATWOOD_GROWER = TreeGrower("greatwood", Optional.of(GREATWOOD_FEATURE), Optional.empty(), Optional.empty())

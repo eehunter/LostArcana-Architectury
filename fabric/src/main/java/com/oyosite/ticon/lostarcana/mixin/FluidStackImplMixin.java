ee69f15cb13f2f62e5e0df431934075893e24a7d
Ned Hunter <eehuntercats@gmail.com>
2025-09-03 12:52:27 -0400

    Improve temporary fix for Architectury issue 655.

--- fabric/src/main/java/com/oyosite/ticon/lostarcana/mixin/FluidStackImplMixin.java
+++ fabric/src/main/java/com/oyosite/ticon/lostarcana/mixin/FluidStackImplMixin.java
@@ -1,5 +1,6 @@
 package com.oyosite.ticon.lostarcana.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.architectury.fluid.fabric.FluidStackImpl;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.component.PatchedDataComponentMap;
@@ -14,11 +15,20 @@ public class FluidStackImplMixin {
 
     @Shadow
     public PatchedDataComponentMap components;
+    //Lnet/minecraft/core/component/PatchedDataComponentMap;asPatch()Lnet/minecraft/core/component/DataComponentPatch;
+
+    /*@ModifyExpressionValue(method = "getPatch()Lnet/minecraft/core/component/DataComponentPatch;", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/component/PatchedDataComponentMap;asPatch()Lnet/minecraft/core/component/DataComponentPatch;"))
+    DataComponentPatch fixPatch(DataComponentPatch original){
+        //if(components.isEmpty())return original;
+        var builder = DataComponentPatch.builder();
+        components.forEach(builder::set);
+        return builder.build();
+    }*/
 
     @Inject(method = "getPatch()Lnet/minecraft/core/component/DataComponentPatch;", at = @At("RETURN"), cancellable = true)
     void captureComponentsDebug(CallbackInfoReturnable<DataComponentPatch> cir){
         //System.out.println("Patch: " + cir.getReturnValue());
-        if(cir.getReturnValue().isEmpty()) {
+        if(!components.isEmpty() && cir.getReturnValue().isEmpty()) {
             var builder = DataComponentPatch.builder();
             components.forEach(builder::set);
             cir.setReturnValue(builder.build());

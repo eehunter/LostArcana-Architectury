5c34f042df143401e95359803527b700ffbd565f
Ned Hunter <eehuntercats@gmail.com>
2025-12-01 21:48:33 -0500

    Add basic funcitonality to Flux Scrubber.

--- common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/scrubber/ScrubberBaseBlock.kt
+++ common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/scrubber/ScrubberBaseBlock.kt
@@ -1,10 +1,14 @@
 package com.oyosite.ticon.lostarcana.block.scrubber
 
+import com.oyosite.ticon.lostarcana.blockentity.FLUX_SCRUBBER_BLOCK_ENTITY
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class ScrubberBaseBlock(properties: Properties) : Block(properties), EntityBlock {
@@ -14,4 +18,12 @@ class ScrubberBaseBlock(properties: Properties) : Block(properties), EntityBlock
     ): BlockEntity = ScrubberBaseBlockEntity(blockPos, blockState)
 
     override fun getRenderShape(blockState: BlockState): RenderShape = RenderShape.ENTITYBLOCK_ANIMATED
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        blockState: BlockState,
+        blockEntityType: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        return if (blockEntityType == FLUX_SCRUBBER_BLOCK_ENTITY.value()) ScrubberBaseBlockEntity as BlockEntityTicker<T> else null
+    }
 }
--- common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/scrubber/ScrubberBaseBlockEntity.kt
+++ common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/scrubber/ScrubberBaseBlockEntity.kt
@@ -1,9 +1,13 @@
 package com.oyosite.ticon.lostarcana.block.scrubber
 
+import com.oyosite.ticon.lostarcana.aura.AuraSource
 import com.oyosite.ticon.lostarcana.blockentity.FLUX_SCRUBBER_BLOCK_ENTITY
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.AABB
 import software.bernie.geckolib.animatable.GeoBlockEntity
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache
 import software.bernie.geckolib.animation.AnimatableManager
@@ -11,8 +15,14 @@ import software.bernie.geckolib.animation.AnimationController
 import software.bernie.geckolib.animation.AnimationState
 import software.bernie.geckolib.animation.RawAnimation
 import software.bernie.geckolib.util.GeckoLibUtil
+import kotlin.math.min
+import kotlin.random.Random
 
 class ScrubberBaseBlockEntity(blockPos: BlockPos, blockState: BlockState) : BlockEntity(FLUX_SCRUBBER_BLOCK_ENTITY.value(), blockPos, blockState), GeoBlockEntity {
+
+    var attachedSource: AuraSource? = null
+
+    val drainAmount = 1f
 
     private val cache: AnimatableInstanceCache = GeckoLibUtil.createInstanceCache(this)
 
@@ -22,7 +32,40 @@ class ScrubberBaseBlockEntity(blockPos: BlockPos, blockState: BlockState) : Bloc
 
     override fun getAnimatableInstanceCache(): AnimatableInstanceCache = cache
 
-    companion object{
+    fun tryConnectSource(){
+        if((attachedSource?.pos?.distanceToSqr(blockPos.above().center) ?: 0.0) > 1.0){
+            attachedSource = null
+        }
+        val l = level?:return
+        if (l.gameTime % 20L != 0L) return
+        if (attachedSource!=null) return
+        attachedSource = l.getBlockEntity(blockPos.above()) as? AuraSource
+        if (attachedSource!=null) return
+        attachedSource = l.getEntities(null, AABB.encapsulatingFullBlocks(blockPos.above(), blockPos.above())) { it is AuraSource }.firstOrNull() as? AuraSource
+    }
+
+    fun onDrain(amount: Float){
+        //TODO
+    }
+
+    companion object: BlockEntityTicker<ScrubberBaseBlockEntity>{
         val ANIMATION = RawAnimation.begin().thenLoop("animation.model.idle")
+
+        override fun tick(
+            level: Level,
+            blockPos: BlockPos,
+            blockState: BlockState,
+            blockEntity: ScrubberBaseBlockEntity
+        ) {
+            blockEntity.tryConnectSource()
+
+            if (level.gameTime % 10L != 0L) return
+            val source = blockEntity.attachedSource?:return
+            if (Random.nextFloat() > .25f) return
+            val f = source.flux
+            val d = min(blockEntity.drainAmount, f)
+            source.flux -= d
+            blockEntity.onDrain(d)
+        }
     }
 }

b9492fa14fa3ce8dfa219c1efa6977df556a830b
Ned Hunter <eehuntercats@gmail.com>
2025-12-05 17:21:36 -0500

    Cleanup GeckoLib renderer registration code on Fabric.

--- fabric/src/main/kotlin/com/oyosite/ticon/lostarcana/fabric/client/LostArcanaFabricClient.kt
+++ fabric/src/main/kotlin/com/oyosite/ticon/lostarcana/fabric/client/LostArcanaFabricClient.kt
@@ -4,46 +4,19 @@ import com.oyosite.ticon.lostarcana.block.INFUSED_STONES
 import com.oyosite.ticon.lostarcana.block.NITOR
 import com.oyosite.ticon.lostarcana.block.VIS_LIGHT
 import com.oyosite.ticon.lostarcana.block.WARDED_JAR
-import com.oyosite.ticon.lostarcana.block.fluid.ESSENTIA_FLUID
-import com.oyosite.ticon.lostarcana.block.fluid.ESSENTIA_FLUID_FLOWING
-import com.oyosite.ticon.lostarcana.block.generator.VisGeneratorBlockEntity
-import com.oyosite.ticon.lostarcana.block.scrubber.ScrubberBaseBlockEntity
-import com.oyosite.ticon.lostarcana.block.virial.VirialNodeBlockEntity
 import com.oyosite.ticon.lostarcana.blockentity.*
 import com.oyosite.ticon.lostarcana.client.LostArcanaClient
-import com.oyosite.ticon.lostarcana.client.blockentity.ArcaneColumnRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.ArcaneWorkbenchScreen
-import com.oyosite.ticon.lostarcana.client.blockentity.CrucibleBlockEntityRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.MagicBricksBlockEntityRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.PedestalRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.ScrubberRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.VirialNodeRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.VisGeneratorRenderer
-import com.oyosite.ticon.lostarcana.client.blockentity.WardedJarRenderer
-import com.oyosite.ticon.lostarcana.item.ESSENTIA_BUCKET_ITEM
-import com.oyosite.ticon.lostarcana.item.GOGGLES_OF_REVEALING
-import com.oyosite.ticon.lostarcana.item.RAW_ASPECT_COMPONENT
-import com.oyosite.ticon.lostarcana.item.THAUMOMETER
-import com.oyosite.ticon.lostarcana.item.VIS_CRYSTAL
-import com.oyosite.ticon.lostarcana.item.WAND_ITEM
+import com.oyosite.ticon.lostarcana.client.blockentity.*
+import com.oyosite.ticon.lostarcana.item.*
 import com.oyosite.ticon.lostarcana.unaryPlus
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry
 import dev.architectury.registry.registries.RegistrySupplier
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
-import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRenderHandler
-import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant
 import net.minecraft.client.gui.screens.MenuScreens
-import net.minecraft.client.particle.ParticleRenderType
 import net.minecraft.client.renderer.RenderType
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
-import net.minecraft.core.BlockPos
-import net.minecraft.world.level.BlockAndTintGetter
 import net.minecraft.world.level.block.Block
-import kotlin.jvm.optionals.getOrNull
 
 class LostArcanaFabricClient : ClientModInitializer {
     override fun onInitializeClient() {
@@ -59,15 +32,15 @@ class LostArcanaFabricClient : ClientModInitializer {
         ColorProviderRegistry.ITEM.register(LostArcanaClient.NITOR_ITEM_COLOR, +NITOR)
         ColorProviderRegistry.ITEM.register(LostArcanaClient.RAW_ASPECTED_ITEM_COLOR, +ESSENTIA_BUCKET_ITEM)
 
-        BlockEntityRendererRegistry.register(MAGIC_BRICKS_BLOCK_ENTITY.value()) { MagicBricksBlockEntityRenderer() as BlockEntityRenderer<MagicBricksBlockEntity> }
-        BlockEntityRendererRegistry.register(ARCANE_COLUMN_BLOCK_ENTITY.value()) { ArcaneColumnRenderer() as BlockEntityRenderer<ArcaneColumnBlockEntity> }
+        BlockEntityRendererRegistry.register(MAGIC_BRICKS_BLOCK_ENTITY.value()) { MagicBricksBlockEntityRenderer() }
+        BlockEntityRendererRegistry.register(ARCANE_COLUMN_BLOCK_ENTITY.value()) { ArcaneColumnRenderer() }
         BlockEntityRendererRegistry.register(RECHARGE_PEDESTAL_BLOCK_ENTITY.value()) { PedestalRenderer<RechargePedestalBlockEntity>() }
         BlockEntityRendererRegistry.register(ARCANE_PEDESTAL_BLOCK_ENTITY.value()) { PedestalRenderer<ArcanePedestalBlockEntity>() }
         BlockEntityRendererRegistry.register(CRUCIBLE_BLOCK_ENTITY.value()) { CrucibleBlockEntityRenderer(it) }
         BlockEntityRendererRegistry.register(WARDED_JAR_BLOCK_ENTITY.value()) { WardedJarRenderer() }
-        BlockEntityRendererRegistry.register(FLUX_SCRUBBER_BLOCK_ENTITY.value()) { ScrubberRenderer() as BlockEntityRenderer<ScrubberBaseBlockEntity> }
-        BlockEntityRendererRegistry.register(VIS_GENERATOR_BLOCK_ENTITY.value()) { VisGeneratorRenderer() as BlockEntityRenderer<VisGeneratorBlockEntity> }
-        BlockEntityRendererRegistry.register(VIRIAL_NODE_BLOCK_ENTITY.value()) { VirialNodeRenderer() as BlockEntityRenderer<VirialNodeBlockEntity> }
+        BlockEntityRendererRegistry.register(FLUX_SCRUBBER_BLOCK_ENTITY.value()) { ScrubberRenderer() }
+        BlockEntityRendererRegistry.register(VIS_GENERATOR_BLOCK_ENTITY.value()) { VisGeneratorRenderer() }
+        BlockEntityRendererRegistry.register(VIRIAL_NODE_BLOCK_ENTITY.value()) { VirialNodeRenderer() }
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), *INFUSED_STONES.map<RegistrySupplier<out Block>, Block>(RegistrySupplier<out Block>::get).toTypedArray())
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.translucent(), WARDED_JAR.value())

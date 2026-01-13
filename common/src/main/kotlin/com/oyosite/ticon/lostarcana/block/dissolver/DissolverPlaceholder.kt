5be261a7ece06bb27539ee225580ce36f40ed743
Ned Hunter <eehuntercats@gmail.com>
2026-01-12 20:34:10 -0500

    Make Dissolver placeholder interactable.

--- common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/dissolver/DissolverPlaceholder.kt
+++ common/src/main/kotlin/com/oyosite/ticon/lostarcana/block/dissolver/DissolverPlaceholder.kt
@@ -2,8 +2,14 @@ package com.oyosite.ticon.lostarcana.block.dissolver
 
 import com.oyosite.ticon.lostarcana.block.MultiblockPlaceholder
 import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.ItemInteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
 
@@ -16,6 +22,19 @@ class DissolverPlaceholder(properties: Properties) : MultiblockPlaceholder(prope
         collisionContext: CollisionContext
     ): VoxelShape {
         return shape
+    }
+
+    override fun useItemOn(
+        itemStack: ItemStack,
+        blockState: BlockState,
+        level: Level,
+        blockPos: BlockPos,
+        player: Player,
+        interactionHand: InteractionHand,
+        blockHitResult: BlockHitResult
+    ): ItemInteractionResult {
+        val be = level.getBlockEntity(blockPos.below()) as? DissolverBlockEntity ?: return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
+        return be.inventoryHelper.useItemOn(itemStack, blockState, level, blockPos, player, interactionHand, blockHitResult)
     }
 
     companion object{
